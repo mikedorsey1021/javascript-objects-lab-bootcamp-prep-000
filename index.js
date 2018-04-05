3aceb548ed492114cdd5df8d0e81d71f1f04cabc
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-05 05:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function deleteFromObjectByKey (object, key){
 }
 
 function destructivelyDeleteFromObjectByKey (object, key){
-  delete recipes[key];
-  return recipes
+  delete object[key];
+  return object
 
 
 }
